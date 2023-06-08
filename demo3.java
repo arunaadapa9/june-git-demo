ef8c0b08cd24926d7212b2c303d876795ab627fc
aruna <aruna.adapa16@gmail.com>
2023-06-08 12:29:37 +0000

    devops changes committed

--- demo3.java
+++ demo3.java
@@ -2,5 +2,8 @@ public class Demo{
 	public static void main(String args[]){
 		//print statement for hello world
 		System.out.println("Hello World");
+		//print statement for devops
+		System.out.println("welcome to devops")
+	
 	}
 }
